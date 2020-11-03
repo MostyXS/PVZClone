d3ce6f5e724ded326f771b104aca2a6884cbe9a7
Mosty <mostseev2013@gmail.com>
2020-11-03 16:37:56 +0300

    Init

---
+++ Assets/Scripts/LivesDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LivesDisplay : MonoBehaviour {
+    [SerializeField] float baseLives = 4;
+    float lives;
+    [SerializeField] float damage;
+    Text livesText;
+
+    void Start()
+    {
+        lives = baseLives - PlayerPrefsController.GetDifficulty();
+        livesText = GetComponent<Text>();
+        UpdateDisplay();
+        Debug.Log("Difficulty lvl =" + PlayerPrefsController.GetDifficulty());
+    }
+
+    private void UpdateDisplay()
+    {
+        livesText.text = lives.ToString();
+    }
+
+    public void TakeLife()
+    {
+        lives -= damage;
+        UpdateDisplay();
+
+        if (lives <= 0)
+        {
+            FindObjectOfType<LevelController>().HandleLoseCondition();
+        }
+    }
+}
---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeRegulator;
+    [SerializeField] float defaultVolume = 0.2f;
+    [SerializeField] Slider difficultySlider;
+    [SerializeField] float defaultDifficulty = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeRegulator.value = PlayerPrefsController.GetMasterVolume();
+        difficultySlider.value = PlayerPrefsController.GetDifficulty();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var musicPlayer = FindObjectOfType<MusicPlayer>();
+        if (musicPlayer)
+        {
+            musicPlayer.SetVolume(volumeRegulator.value);
+        }
+        else 
+            Debug.LogWarning("No music player found...");
+        
+    }
+    public void SaveAndExit()
+    {
+        PlayerPrefsController.SetMasterVolume(volumeRegulator.value);
+        PlayerPrefsController.SetDifficulty(difficultySlider.value);
+        FindObjectOfType<LevelLoader>().LoadMainMenu();
+    }
+
+    public void SetToDefault()
+    {
+        volumeRegulator.value = defaultVolume;
+        difficultySlider.value = defaultDifficulty;
+    }
+
+
+}
